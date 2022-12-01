2c909f688ef57cb64f30bbc1856fb3f117be8607
MarcoAgosta <agostamarco@libero.it>
2022-12-01 16:46:06 +0100

    Cambiate due cose

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="jumbotron p-1">
   <h1 class="display-4">Ciao sono un Jumbotron</h1>
-  <p class="lead">Sono stato fatto principalmente con Bootstrap</p>
+  <p class="lead">Sono stato fatto principalmente con Bootstrap e ho eliminato un po' di elementi dal CSS generale</p>
   <hr class="my-4">
   <p>Il colore di sfondo invece lo ho messo con un elemento CSS, (il pulsante sotto è inutile)</p>
   <p class="lead">

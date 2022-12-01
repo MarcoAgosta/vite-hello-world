049179f4780d0e2e50826336725276bb0c73a604
MarcoAgosta <agostamarco@libero.it>
2022-12-01 15:50:50 +0100

    Creato esercizio base

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script>
+import AppTitle from "./components/AppTitle.vue"
 export default{
-
+  components:{AppTitle}
 }
 </script>
 
 <template>
-
+  <AppTitle></AppTitle>
+  <p>L'ho messo in alto lavorando sul CSS generale</p>
 </template>
 
-<style scoped>
-
+<style>
+h1{
+  color: red;
+}
 </style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>La mia prima App con vite</h1>
+    </div>
+</template>
+
+<script>
+</script>
